0d9ec0a3270b8dff87380b3aa62eb2c8d51de507
Vadim Pislari <46645781+PuPsIkI17@users.noreply.github.com>
2019-03-14 23:16:23 +0200

    Create Cache.java

---
+++ Cache.java
@@ -0,0 +1,11 @@
+//Pislari Vadim 323CB
+
+public interface Cache {
+
+	public abstract void add(Premium value);
+
+	public abstract void remove(Premium value);
+
+	public Premium cauta(String value);
+
+}
